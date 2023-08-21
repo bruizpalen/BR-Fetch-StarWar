b5748a4c1a2079708f0464b00e7d2a629426c674
bruizpalen <115789234+bruizpalen@users.noreply.github.com>
2023-08-21 20:52:35 +0000

    First exercise

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,72 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const [people, setPeople] = useState([]);
+  const [property, setProperty] = useState([]);
 
+  // const getProperties = (index, selectedProperty) => {
+  //   fetch(`https://www.swapi.tech/api/people/${index}`)
+  //     .then((res) => {
+  //       if (!res.ok) throw Error(res.statusText);
+  //       return res.json();
+  //     })
+  //     .then((prop) => prop.result.properties[selectedProperty]) // -- ?? setProperty(prop.result.properties[selectedProperty])
+  //     .catch((error) => console.log("error", error));
+  // };
+
+  useEffect(() => {
+    fetch("https://www.swapi.tech/api/people")
+      .then((res) => {
+        if (!res.ok) throw Error(res.statusText);
+        return res.json();
+      })
+      .then((response) => {
+        setPeople(response.results);
+
+        const listPromises = response.results.map((person) => {
+          return fetch(`https://www.swapi.tech/api/people/${person.uid}`)
+            .then((res) => {
+              if (!res.ok) throw Error(res.statusText);
+              return res.json();
+            })
+            .then((prop) => {
+              console.log(prop);
+              return prop.result.properties;
+            })
+            .catch((error) => console.log("error", error));
+        });
+        return Promise.all(listPromises)
+      })
+	  .then((listPro) => console.log(listPro))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {people.map((element) => (
+          <li key={element.uid}>{element.name}</li>
+        ))}
+      </ul>
+      {/* <ul>
+        {property.map((proper, index) => (
+          <li key={index}>{proper}</li>
+        ))}
+      </ul> */}
+    </div>
+  );
+};
 export default Home;
+
+{
+  /* <p>
+              {getProperties(element.uid, "eye_color")
+                .then((propertyEye) => propertyEye)
+                .catch((error) => console.log("error", error))}
+            </p> */
+}
+{
+  /* {console.log(typeof getProperties(element.uid, "eye_color"))} */
+}
